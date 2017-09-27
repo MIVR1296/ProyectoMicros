22e15d24fda823ee81f23b57efe3300a1a1c4c21
Jeangh95 <jeangh95@gmail.com>
2017-09-26 21:50:43 -0600

    Update Memoria.v

--- Memoria.v
+++ Memoria.v
@@ -29,34 +29,39 @@ module Memoria (clk, mem_read, mem_write, address, data_in, data_out);
   
   reg    [31:0] data_out;
 
-  parameter BASE_ADDRESS = 25'd0; // address that applies to this memory - change if desired
+  parameter BASE_ADDRESS = 25'd0; 
 
-  reg [31:0] mem_array [0:31];
+  reg [31:0] mem_array [0:31]; 
   wire [4:0] mem_offset;
   wire address_select;
 
-  assign mem_offset = address[6:2];  // drop 2 LSBs to get word offset
+  assign mem_offset = address[6:2];  // Se toman 2 LSB para el deesplazamiento de los words ( en caso de ser necesario)
 
-  assign address_select = (address[31:7] == BASE_ADDRESS);  // address decoding
-
+  assign address_select = (address[31:7] == BASE_ADDRESS);  // se decodifica la dirección
+  
+  //****************************************************************************
+  //                      Para la lectura de memoria
+  //****************************************************************************
   always @(mem_read or address_select or mem_offset or mem_array[mem_offset])
   begin
 
-    if (mem_read == 1'b1 && address_select == 1'b1)
+    if (mem_read == 1'b1 && address_select == 1'b1) // Si la señal de control de lectura está activa y el selector de dirección también lo está
     begin
-      if ((address % 4) != 0) // Si la dirección no es múltiplo de 4 entonces
+      if ((address % 4) != 0) // Entonces se evalua si la dirección no es múltiplo de 4 para desplegar el error
           $display($time, " rom32 error: unaligned address %d", address);
           data_out = mem_array[mem_offset];
           $display($time, " reading data: Mem[%h] => %h", address, data_out);
       end
-      else data_out = 32'hxxxxxxxx;
+      else data_out = 32'hxxxxxxxx; // si la dirección es múltiplo de 4 entonces el dato de salida es por defecto, pues estamos en una lectura
   end
 
-  // for WRITE operations
-  always @(posedge clk)
+  //****************************************************************************
+  //                      Para la escritura de memoria
+  //****************************************************************************
+  always @(posedge clk) // Siempre que estemos en un flanco de reloj positivo
   begin
-    if (mem_write == 1'b1 && address_select == 1'b1)
-    begin
+    if (mem_write == 1'b1 && address_select == 1'b1) // Si la señal de control de escritura y el selector de dirección está activo
+      begin // entonces:
       // $display("<format>", exp1, exp2, ...); 
       //El primer grupo de tareas de visualización es muy similar a imprimir la función en el lenguaje ANSI C (la sintaxis es casi la misma). 
       //La tareas $ display añade un nuevo carácter de línea al final de la salida.
@@ -65,7 +70,7 @@ module Memoria (clk, mem_read, mem_write, address, data_in, data_out);
     end
   end
 
-  // initialize with some arbitrary values
+  // Se inicializa con valores arbitrarios el mem_array
 
   integer i;
   initial begin
